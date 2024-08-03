56dc2e34b4b1779310c9504d7a1ced55457e3722
ArtemVoinPeruna <artemlavrentev578@gmail.com>
2024-08-04 02:22:11 +0300

    Небольшие изменения :>

---
+++ Assets/Scripts/Core/RewardLine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Rewards
+{
+    public class RewardLine : MonoBehaviour
+    {
+        private int _coin = 0;
+
+        public int Coins
+        {
+            get { return _coin; }
+            private set { _coin = value; }
+        }
+
+        public void AddCoins()
+        {
+            Coins++;
+        }
+
+        public void CollectionsCoin()
+        {
+            Coins = 0;
+        }
+
+        public void KnopkaSbora()
+        {
+            CollectionsCoin();
+        }
+    }
+
+}
