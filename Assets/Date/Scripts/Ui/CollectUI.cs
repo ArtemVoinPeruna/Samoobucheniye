313073533e29a570d4f8cd61e9d0a66af61f8cfd
cyraqebk <80762369+cyraqebk@users.noreply.github.com>
2024-09-10 23:01:29 +0300

    interface bar
    po huine

--- Assets/Date/Scripts/Ui/CollectUI.cs
+++ Assets/Date/Scripts/Ui/CollectUI.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using Core.Rewards;
+using UnityEngine.UI;
 
 namespace UI.RewardUIs
 {
     public class CollectUI : MonoBehaviour
     {
         [SerializeField] private RewardView _rewardView;
+       // [SerializeField] Button b
     }
 }
---
+++ Assets/Date/Scripts/Ui/InterfaceBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Rewards;
+using TMPro;
+
+namespace UI.RewardUIs
+{
+    public class InterfaceBar : MonoBehaviour
+    {
+        [field: SerializeField] private TMP_Text _text;
+        [field: SerializeField] private RewardView _rewardView;
+        private void OnEnable() 
+            {
+                _rewardView.BarChanged += OnBarChange;
+            }
+
+            private void OnDisable() 
+            {
+                _rewardView.BarChanged += OnBarChange;
+            }
+        public void OnBarChange(RewardLine bar)
+        {
+        
+        }    
+    }
+}
